50d4a39cd298aefac77073ecbfe5461806771ed2
David Hopkins <dlhopkins2@gmail.com>
2018-07-05 16:50:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
-  let greeting;
+  let response
   if (someValue <= 400) {
-    greeting = 'This one is on me!'
+    response = 'This one is on me!'
   }
+  else if (someValue > 2000 && someValue <= 2500) {
+    response = 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500) {
+    response = 'No can do.'
 }
